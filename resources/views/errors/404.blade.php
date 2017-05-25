5b18ceb4d0ccb44d8f5893df7ea5f27f6ec57bc3
Steve McKeogh <steve.mckeogh@gmail.com>
2017-05-25 12:01:34 +0100

    404 added

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.www')
+@section('body_class', 'ErrorPage')
+
+@section('main')
+    
+    <div class="Main"><div class="Spotlight"><div class="Wrapper">
+        <h1 class="H1">404 - Missing Page?</h1>
+        <p>Sorry, we couldn't find the page you were looking for.</p>
+        <p>We'll keep looking - you check out the rest of the site in the meantime.</p>
+    </div></div></div>
+    
+@endsection
